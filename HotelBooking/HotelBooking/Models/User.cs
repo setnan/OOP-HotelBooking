cd1f865f1c8e7175c11b1495b261f9c730387cd7
MarcusBrustad <brustadmarcus@gmail.com>
2025-03-28 09:26:09 +0100

    Added details to user

---
+++ HotelBooking/HotelBooking/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelBooking.Models;
+
+public enum Role
+{
+    Admin,
+    Receptionist
+}
+public class User(int UserID, string name, string email, string password)
+{
+    public int UserID { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Role Role { get; set; }
+    
+}
