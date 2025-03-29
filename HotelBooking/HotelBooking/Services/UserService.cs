c4f9aa48334809619d498462483ab91793d0b4e0
MarcusBrustad <brustadmarcus@gmail.com>
2025-03-29 17:09:19 +0100

    FEAT: Lagde UserService, med noen start funksjoner. Begynt å CRUD

---
+++ HotelBooking/HotelBooking/Services/UserService.cs
@@ -0,0 +1,43 @@
+using HotelBooking.Models;
+
+namespace HotelBooking.Services;
+
+public static class UserService
+{
+    public static bool IsAdmin(User user)
+    {
+        return  user.Role == Role.Admin;
+    }
+
+    public static void UpdateUserEmail(int userId, string email)
+    {
+        var query = "UPDATE User SET Email = @email WHERE UserId = @userId";
+        
+        DatabaseConnection.Instance.ExecuteSql(query, new {email, userId});
+    }
+
+    public static void UpdateUserPassword(int userId, string password)
+    {
+        var query = "UPDATE User SET Password = @password WHERE UserId = @userId";
+        DatabaseConnection.Instance.ExecuteSql(query, new {password, userId});
+    }
+
+    public static List<User> GetAllUsers()
+    {
+        return DatabaseConnection.Instance.GetAll<User>("Users");
+    }
+
+    public static void AddUser(User user)
+    {
+        string insertQuery = @"INSERT INTO User (Name, Email, Password, Role)  VALUES (@name, @email, @password, @role)";
+        DatabaseConnection.Instance.ExecuteSql(insertQuery, user);
+    }
+    
+    public static User? GetUserFromEmail(string email)
+    {
+        var query = @"SELECT * FROM User WHERE Email = @email";
+        return DatabaseConnection.Instance.GetOne<User>(query, new { email });
+    }
+
+
+}
