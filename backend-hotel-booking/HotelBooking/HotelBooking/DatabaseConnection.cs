60db60c69059d4a12b972f3c3261da7fddb34898
setnan <111985341+setnan@users.noreply.github.com>
2025-04-03 11:46:09 +0200

    FIX: endret DatabaseConnection for klargjøring til postgres og render integrasjon

--- backend-hotel-booking/HotelBooking/HotelBooking/DatabaseConnection.cs
+++ backend-hotel-booking/HotelBooking/HotelBooking/DatabaseConnection.cs
@@ -1,6 +1,7 @@
 ﻿using HotelBooking.Models;
-using MySql.Data.MySqlClient;
 using Dapper;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
 
 namespace HotelBooking;
 
@@ -8,26 +9,24 @@ public class DatabaseConnection
 {
     private static DatabaseConnection? _instance;
     public static DatabaseConnection Instance => _instance ??= new DatabaseConnection();
-    
-    private readonly MySqlConnection _connection;
-    private readonly string _connectionString = "server=localhost;" +
-                                       "port=3306;" +
-                                       "database=HotelBooking;" +
-                                       "uid=hotelluser;" +
-                                       "pwd=hotellpass;";
+
+    private readonly NpgsqlConnection _connection;
 
     private DatabaseConnection()
     {
-        _connection = new MySqlConnection(_connectionString);
+        var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+                               ?? throw new InvalidOperationException("Missing CONNECTION_STRING");
+
+        _connection = new NpgsqlConnection(connectionString);
     }
 
     public void Open() => _connection.Open();
     public void Close() => _connection.Close();
 
-    public List<T> ExecuteQuery<T>(string query, Func<MySqlDataReader, T> map)
+    public List<T> ExecuteQuery<T>(string query, Func<NpgsqlDataReader, T> map)
     {
         var result = new List<T>();
-        using var command = new MySqlCommand(query, _connection);
+        using var command = new NpgsqlCommand(query, _connection);
         using var reader = command.ExecuteReader();
         while (reader.Read())
         {
@@ -38,34 +37,29 @@ public class DatabaseConnection
 
     public void ExecuteNonQuery(string query)
     {
-        using var command = new MySqlCommand(query, _connection);
+        using var command = new NpgsqlCommand(query, _connection);
         command.ExecuteNonQuery();
     }
-    
+
     public void ExecuteSql(string sql, object parameters)
     {
         _connection.Execute(sql, parameters);
     }
-    
+
     public object? ExecuteQueryRow(string mail)
     {
-        
-        var query = "SELECT * FROM User WHERE Email = @email";
-        var user = _connection.QuerySingleOrDefault<User>(query, new { email = mail });
-        return user;
+        const string query = "SELECT * FROM \"User\" WHERE \"Email\" = @email";
+        return _connection.QuerySingleOrDefault<User>(query, new { email = mail });
     }
 
     public List<T> GetAll<T>(string tableName)
     {
-        var query = "SELECT * FROM " + tableName;
+        var query = $"SELECT * FROM \"{tableName}\"";
         return _connection.Query<T>(query).ToList();
     }
-    
+
     public T? GetOne<T>(string sql, object parameters)
     {
         return _connection.QuerySingleOrDefault<T>(sql, parameters);
     }
-
-    
-    
 }
