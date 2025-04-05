18e76490e130c3fddfb33f52973de0e58080e4d0
setnan <111985341+setnan@users.noreply.github.com>
2025-04-05 11:14:55 +0200

    FIX: la til PK felt for Catering.cs

--- backend-hotel-booking/HotelBooking/HotelBooking/Models/Catering.cs
+++ backend-hotel-booking/HotelBooking/HotelBooking/Models/Catering.cs
@@ -1,12 +1,15 @@
-﻿using System;
-
-namespace HotelBooking.Models;
+﻿using HotelBooking.Models;
 
 public class Catering
 {
     public int CateringId { get; set; }
+
+    public int ClientId { get; set; } // <-- NY
+    public int RoomId { get; set; }   // <-- NY
+
     public Client Organiser { get; set; }
     public Room Room { get; set; }
+
     public DateTime Date { get; set; }
     public TimeSpan StartTime { get; set; }
     public TimeSpan EndTime { get; set; }
