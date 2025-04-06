c881c4339cda868d60533a8c4484d2794c41e772
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-06 12:14:39 +0200

    Lagt til Crud metoder som jobber med nyeste CRUD i databaseconnection

--- backend-hotel-booking/HotelBooking/HotelBooking/Services/RoomService.cs
+++ backend-hotel-booking/HotelBooking/HotelBooking/Services/RoomService.cs
@@ -1,42 +1,48 @@
 ﻿using System.Collections;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 using HotelBooking.Database;
 using HotelBooking.Models;
+using Newtonsoft.Json;
 
 namespace HotelBooking.Services;
 
 public class RoomService(DatabaseConnection db)
 {
-    // public static List<Room> GetAvailableRooms()
-    // {
-    //     string query = "SELECT * FROM Room WHERE is_available = 1";
-    //
-    //     return _db.ExecuteQuery(query, reader => new Room
-    //     {
-    //         RoomId = reader.GetInt32("room_id"),
-    //         HotelId = reader.GetInt32("hotel_id"),
-    //         RoomNumber = reader.GetString("room_number"),
-    //         Type = reader.GetString("type"),
-    //         Price = reader.GetDecimal("price"),
-    //         IsAvailable = reader.GetBoolean("is_available")
-    //     });
-    // }
-
-    public static void AddRoom(Room room)
+    public static bool AddRoom(Room room)
     {
-        var insertQuery = @"
-        INSERT INTO Room (HotelId, RoomNumber, Type, Price, IsAvailable) VALUES (@HotelId, @RoomNumber, @Type, @Price,  @IsAvailable)";
-        DatabaseConnection.Instance.ExecuteSql(insertQuery, room);
+        return DatabaseConnection.Instance.Insert(room);
+    }
+    
+    public static bool UpdateRoom(Room room, string json)
+    {
+        var updatedRoomData = JsonConvert.DeserializeObject<Room>(json);
+        foreach (var property in typeof(Room).GetProperties())
+        {
+            var newProperty = property.GetValue(updatedRoomData);
+            if (newProperty != null && !newProperty.Equals(property.GetValue(room)))
+            {
+                property.SetValue(room, newProperty);
+            }
+        }
+        return DatabaseConnection.Instance.Update(room);
+    }
 
+    public static bool DeleteRoom(Room room)
+    {
+        return DatabaseConnection.Instance.Delete(room);
     }
 
     public static List<Room> GetAllRooms()
     {
-        return DatabaseConnection.Instance.GetAll<Room>("Room");
+        return DatabaseConnection.Instance.GetAll<Room>();
     }
 
 
-    public IEnumerable GetAvailableRooms()
+    public static List<Room> GetAvailableRooms()
     {
-        throw new NotImplementedException();
+        var rooms = GetAllRooms();
+        return rooms.Where(x => x.IsAvailable).ToList();
     }
+    
 }
