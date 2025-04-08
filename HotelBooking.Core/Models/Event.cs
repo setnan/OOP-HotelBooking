76a4b7aeb83908801e96126d1f0d95d95c3c972e
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-09 00:42:58 +0200

    FIX: Oppdaterte event til å bruke EventRoom og Eventclient med getRooms og GetClientMetoder

--- HotelBooking.Core/Models/Event.cs
+++ HotelBooking.Core/Models/Event.cs
@@ -1,13 +1,23 @@
-﻿namespace HotelBooking.Core.Models;
+﻿using HotelBooking.Core.Services;
+
+namespace HotelBooking.Core.Models;
 
 public class Event
 {
     public int EventId { get; set; }
-    public Client Organiser { get; set; }
-    public List<Room> Rooms { get; set; } = new();
+    
+    //Navigation properties for reading / mapping
+    private List<EventClient> EventClients = new();
+    private List<EventRoom> EventRooms = new();
+    
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public TimeSpan StartTime { get; set; }
     public TimeSpan EndTime { get; set; }
+
+
+    public List<EventClient>? GetEventClients() => EventService.GetClientsByEventId(EventId);
+    public List<EventRoom>? GetEventRooms() => EventService.GetRoomsByEventId(EventId);
+
     
 }
