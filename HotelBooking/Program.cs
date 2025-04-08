1e294f19c682950d32c529ccde983b7cadb188c4
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-08 20:39:15 +0200

    Test for insert osv.

--- HotelBooking/Program.cs
+++ HotelBooking/Program.cs
@@ -8,3 +8,27 @@ foreach (var user in users)
 {
     Console.WriteLine(user.Dump());
 }
+
+List<Room> availableRooms = RoomService.GetAvailableRooms(DateTime.Parse("2025-04-02"), DateTime.Parse("2025-04-04"));
+foreach (var room in availableRooms)
+{
+    Console.WriteLine(room.Dump());
+}
+
+var newBooking = new Booking
+{
+    GuestId = 1,
+    RoomId = 1,
+    CheckIn = DateTime.Parse("2025-04-02"),
+    CheckOut = DateTime.Parse("2025-04-04")
+};
+BookingService.AddBooking(newBooking);
+
+
+BookingService.AddBooking(newBooking);
+
+List<Room> availableRooms1 = RoomService.GetAvailableRooms(DateTime.Parse("2025-04-02"), DateTime.Parse("2025-04-04"));
+foreach (var room in availableRooms1)
+{
+    Console.WriteLine(room.Dump());
+}
