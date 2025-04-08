cb166922752ef6d9c99d8c03c904d4d271e8f9e0
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-08 14:01:19 +0200

    endret i program bare

--- HotelBooking/Program.cs
+++ HotelBooking/Program.cs
@@ -1,9 +1,7 @@
 ﻿using Dumpify;
-using HotelBooking.Core.Database;
 using HotelBooking.Core.Services;
 using HotelBooking.Core.Models;
-using HotelBooking.Core.Services;
-using HotelBooking.Core.Models;
+
 
 List<User> users = UserService.GetAllUsers();
 foreach (var user in users)
