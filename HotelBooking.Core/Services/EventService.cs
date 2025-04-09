98c9e61399d165fbe6f0dbaa08e9e7eae1a5c5c3
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-09 02:00:36 +0200

    Lagt til CreateEvent som lager et event, så oppdaterer linktabeller med riktig info

--- HotelBooking.Core/Services/EventService.cs
+++ HotelBooking.Core/Services/EventService.cs
@@ -1,4 +1,5 @@
-﻿using HotelBooking.Core.Database;
+﻿using Dapper;
+using HotelBooking.Core.Database;
 using HotelBooking.Core.Models;
 
 namespace HotelBooking.Core.Services;
@@ -9,6 +10,47 @@ public class EventService(DatabaseConnection connection)
     public static bool AddEvent(Event thisevent)
     {
         return DatabaseConnection.Instance.Insert(thisevent);
+    }
+
+    public static bool CreateEvent(Event thisevent, List<string> clientBillingAddress, List<string> roomNumbers)
+    {
+        if (!AddEvent(thisevent)) return false;
+        var lastEvent = GetEventByNameAndDate(thisevent.Name, thisevent.StartDate);
+        
+        if (lastEvent == null) return false;
+
+        foreach (var billingaddress in clientBillingAddress)
+        {
+            Client? client = ClientService.GetClientByBillingAddress(billingaddress);
+            if (client == null) return false;
+            EventClient newEventClient = new EventClient
+            {
+                EventId = lastEvent.EventId,
+                ClientId = client.ClientId
+            };
+            DatabaseConnection.Instance.Insert(newEventClient);
+        }
+
+        foreach (var roomNumber in roomNumbers)
+        {
+            Room? room = RoomService.GetRoomByNumber(roomNumber);
+            if (room == null) return false;
+            EventRoom newEventRoom = new EventRoom
+            {
+                EventId = lastEvent.EventId,
+                RoomId = room.RoomId,
+            };
+            DatabaseConnection.Instance.Insert(newEventRoom);
+        }
+        
+        return true;
+    }
+
+    public static Event? GetEventByNameAndDate(string name, DateTime startDate)
+    {
+        var connection = DatabaseConnection.Instance.GetConnection();
+        var query = @"SELECT * FROM Event WHERE Name = @Name AND StartDate = @StartDate";
+        return connection.QuerySingleOrDefault<Event>(query, new { Name = name, StartDate = startDate });
     }
 
     public static bool UpdateEvent(Event thisevent)
