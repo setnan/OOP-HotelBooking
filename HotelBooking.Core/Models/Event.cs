55904e183f9a0f34a2007aed1773fc912fffd378
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-09 12:37:01 +0200

    Fikset til å fungere med ikke statisk

--- HotelBooking.Core/Models/Event.cs
+++ HotelBooking.Core/Models/Event.cs
@@ -6,39 +6,13 @@ public class Event
 {
     public int EventId { get; set; }
     public string Name { get; set; }
-    
-    //Navigation properties for reading / mapping
     public string HotelId { get; set; }
-    public List<EventClient> EventClients { get; private set; } = new();
-    public List<EventRoom> EventRooms { get; private set; } = new();
-    
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public TimeSpan StartTime { get; set; }
     public TimeSpan EndTime { get; set; }
 
-
-    public async Task<List<EventClient>?> GetEventClients() =>  await EventService.GetClientsByEventIdAsync(EventId);
-    public void AddEventClient(EventClient client)
-    {
-        EventClients.Add(client);
-    }
-    public void AddAllEventClients(List<EventClient> clients)
-    {
-        EventClients = clients;
-    }
-    
-    public async Task<List<EventRoom>?> GetEventRooms() => await EventService.GetRoomsByEventIdAsync(EventId);
-
-    public void AddEventRoom(EventRoom room)
-    {
-        EventRooms.Add(room);
-    }
-
-    public void AddAllEventRooms(List<EventRoom> rooms)
-    {
-        EventRooms = rooms;
-    }
-
-    
+    // Relasjoner, data-only
+    public List<EventClient> EventClients { get; set; } = new();
+    public List<EventRoom> EventRooms { get; set; } = new();
 }
