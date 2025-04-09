af0c141915df96d47e1009e9089a05fe5b836f0d
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-09 10:13:46 +0200

    Kode for å teste event

--- HotelBooking/Program.cs
+++ HotelBooking/Program.cs
@@ -3,43 +3,49 @@ using HotelBooking.Core.Database;
 using HotelBooking.Core.Services;
 using HotelBooking.Core.Models;
 
-Console.WriteLine("Get BY ID ");
-var client = ClientService.GetClientById(8);
-Console.WriteLine(client.Dump());
 
-var newClient = new Client
+var EventInfos = EventService.GetEventsWithDetails();
+foreach (var eventInfo in EventInfos)
 {
-    Name = "John Doe & co",
-    BillingAddress = "test@gmail.com",
-    ContactPerson = "John Doe",
-    ContactNumber = "456-123-456"
-};
-ClientService.AddClient(newClient);
-var clients = ClientService.GetAllClients();
-Console.WriteLine($"\nAdd Client");
-foreach (var clientel in clients)
-{
-    Console.WriteLine(clientel.Dump());
+    Console.WriteLine(eventInfo.Dump());
 }
-
-Console.WriteLine("\nCHECKPOINT!");
-var getClient = ClientService.GetClientById(11);
-
-if (getClient != null) getClient.Name = "John Doe & co & co";
-if (getClient != null) ClientService.UpdateClient(getClient);
-var getClients = ClientService.GetAllClients();
-Console.WriteLine($"\nUpdate Client");
-foreach (var newclient in getClients)
-{
-    Console.WriteLine(newclient.Dump());
-}
-
-ClientService.DeleteClient(getClient);
-
-var gottenClients = ClientService.GetAllClients();
-Console.WriteLine($"\nDelete Client");
-foreach (var newclientell in gottenClients)
-{
-    Console.WriteLine(newclientell.Dump());
-}
+// Console.WriteLine("Get BY ID ");
+// var client = ClientService.GetClientById(8);
+// Console.WriteLine(client.Dump());
+//
+// var newClient = new Client
+// {
+//     Name = "John Doe & co",
+//     BillingAddress = "test@gmail.com",
+//     ContactPerson = "John Doe",
+//     ContactNumber = "456-123-456"
+// };
+// ClientService.AddClient(newClient);
+// var clients = ClientService.GetAllClients();
+// Console.WriteLine($"\nAdd Client");
+// foreach (var clientel in clients)
+// {
+//     Console.WriteLine(clientel.Dump());
+// }
+//
+// Console.WriteLine("\nCHECKPOINT!");
+// var getClient = ClientService.GetClientById(11);
+//
+// if (getClient != null) getClient.Name = "John Doe & co & co";
+// if (getClient != null) ClientService.UpdateClient(getClient);
+// var getClients = ClientService.GetAllClients();
+// Console.WriteLine($"\nUpdate Client");
+// foreach (var newclient in getClients)
+// {
+//     Console.WriteLine(newclient.Dump());
+// }
+//
+// ClientService.DeleteClient(getClient);
+//
+// var gottenClients = ClientService.GetAllClients();
+// Console.WriteLine($"\nDelete Client");
+// foreach (var newclientell in gottenClients)
+// {
+//     Console.WriteLine(newclientell.Dump());
+// }
 
