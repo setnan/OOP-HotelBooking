c09143164847ee2bcffef247ddd39416a0a91dda
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-09 12:38:56 +0200

    Fikset til å fungere ikke statisk

--- HotelBooking.Core/Models/Booking.cs
+++ HotelBooking.Core/Models/Booking.cs
@@ -5,19 +5,9 @@ namespace HotelBooking.Core.Models;
 public class Booking
 {
     public int BookingId { get; set; }
-
-    // Foreign keys for insert/update
     public int GuestId { get; set; }
     public int RoomId { get; set; }
-
-    // Navigation properties for reading / mapping
-    private Guest _guest;
-    private Room _room;
-    
     public DateTime CheckIn { get; set; }
     public DateTime CheckOut { get; set; }
-    
-    public async Task<Guest?> GetGuestAsync() => await GuestService.GetGuestByIdAsync(GuestId);
-    public async Task<Room?> GetRoomAsync() => await RoomService.GetRoomByIdAsync(RoomId);
-
 }
+
