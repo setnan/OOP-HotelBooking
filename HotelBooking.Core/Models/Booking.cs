90c9b9e657de35d83a04b8d53b5f264e224cdfa7
setnan <111985341+setnan@users.noreply.github.com>
2025-04-10 09:48:43 +0200

    FIX: la til guest og room i booking.cs

--- HotelBooking.Core/Models/Booking.cs
+++ HotelBooking.Core/Models/Booking.cs
@@ -9,5 +9,7 @@ public class Booking
     public int RoomId { get; set; }
     public DateTime CheckIn { get; set; }
     public DateTime CheckOut { get; set; }
+    public Guest? Guest { get; set; }
+    public Room? Room { get; set; }
 }
 
