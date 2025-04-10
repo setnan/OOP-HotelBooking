d1a1abd6068c649ee4f3943afd85af0af06db8ec
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-11 00:39:00 +0200

    Feat: Meal Model klasse

---
+++ HotelBooking.Core/Models/Meal.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace HotelBooking.Core.Models;
+
+public enum MealEnum
+{
+    Breakfast,
+    Lunch,
+    Dinner
+}
+public class Meal
+{
+    public int MealId { get; set; }
+    public string Name { get; set; }
+    public DateTime Date  { get; set; }
+    public MealEnum MealType { get; set; }
+}
