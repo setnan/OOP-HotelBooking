be5fca60f91bc5de8614166bda78d3e2aad1d0e8
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-10 23:52:10 +0200

    La til metode for å slette alle fra en tabell..

--- HotelBooking.Core/Database/DatabaseConnection.cs
+++ HotelBooking.Core/Database/DatabaseConnection.cs
@@ -117,4 +117,22 @@ public class DatabaseConnection
 
         return (await _connection.QueryAsync<Room>(query, new { checkIn, checkOut })).ToList();
     }
+
+    public async Task DeleteAllAsync<T>()
+    {
+        var table = GetTableName<T>();
+        var query = $"DELETE FROM {table}";
+        await _connection.ExecuteAsync(query);
+    }
+/*
+var table = GetTableName<T>();
+var query = $"SELECT * FROM {table}";
+return (await _connection.QueryAsync<T>(query)).ToList();
+ * 
+var table = GetTableName<T>();
+var deleteQuery = $"DELETE FROM {table} WHERE {table}Id = @{table}Id";
+var rowsAffected = await _connection.ExecuteAsync(deleteQuery, entity);
+return rowsAffected > 0;
+ */
+
 }
