721003ff174d89bee8312e2699c7780faa514a64
MarcusBrustad <brustadmarcus@gmail.com>
2025-04-25 12:32:32 +0200

    Added clean 'EventDbWriter' to have a clean Event for db insertion and updates etc without the lists that are in the 'Event' model.

---
+++ HotelBooking.Core/Models/EventDBWriter.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelBooking.Core.Models;
+
+public class EventDBWriter
+{
+    public int EventId { get; set; }
+    public string Name { get; set; }
+    public int HotelId { get; set; }
+    public int OrganiserId { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+}
